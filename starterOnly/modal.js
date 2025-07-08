42ef98c8bc10c940b3819ce8f82f72a1ef9631b8
aze <aze3@mail.fr>
2025-07-08 09:14:53 +0200

    Issue #1: Fermer la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,18 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalCloseBtn = document.querySelector("#modal-close-btn");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalCloseBtn && modalCloseBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+function closeModal() {
+  modalbg.style.display = "none";
+}
